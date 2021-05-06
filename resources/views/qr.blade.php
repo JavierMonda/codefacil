8348cdb56754079d3a64f59ad9d3f2ad99517cd6
javiermonda <javier.raidpc@gmail.com>
2021-05-06 21:05:09 +0100

    Actualización para mostrar correctamente PDF

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -16,8 +16,10 @@
 					<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=codefacil-21&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B072LPTBBC&linkId=92f67dfe9f6df669031f8b4160a05ee3"></iframe>
                     <p>Su publicidad aquí (<a class="a-footer" href="https://javierjg.es/blog/contacto/" target="_blank">contacto</a>)</p>
 
-                    <object data="{{ url('/') }}/{{$user->file}}" type="application/pdf"></object>
-					<!--<embed src="{{ url('/') }}/{{$user->file}}"></embed>-->
+                    <div class="doc-container">
+                        <iframe src="https://drive.google.com/viewerng/viewer?url={{ url('/') }}/{{$user->file}}&embedded=true" frameborder="0" width="100%" height="100%"></iframe>
+                    </div>
+
                 </div>
             </div>
         </div>

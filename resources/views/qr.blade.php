b03333d23b61476a8b524918cd9ec7e434159899
javiermonda <javier.raidpc@gmail.com>
2021-05-07 05:59:51 +0100

    Modificación estilos: a-contacto

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -13,7 +13,7 @@
 
                     @include('custom.message')
 					<a href="https://ordenatech.es" target="_blank"><img src="images/publicity/banner_Ordenatech.jpg" alt="Ordenatech" /></a>
-                    <p>Su publicidad aquí (<a class="a-footer" href="https://javierjg.es/blog/contacto/" target="_blank">contacto</a>)</p>
+                    <p>Su publicidad aquí (<a class="a-contacto" href="https://javierjg.es/blog/contacto/" target="_blank">contacto</a>)</p>
 
                     <div class="doc-container">
                         <iframe src="https://drive.google.com/viewerng/viewer?url={{ url('/') }}/{{$user->file}}&embedded=true" frameborder="0" width="100%" height="100%"></iframe>
